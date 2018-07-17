5665cc3e6ff8905b54fdd0cce04480d816dd2f19
kazedayo <winson883@gmail.com>
2018-07-17 18:57:16 +0800

    remove unused js

--- GaldenApp/redrawImg.js
+++
@@ -1,6 +0,0 @@
-function redrawImg(obj,img){
-	obj.fadeOut(200,function(){
-		obj.attr('src',img);
-		obj.attr('onclick', 'window.webkit.messageHandlers.imageView.postMessage(\''+img+'\')')
-	}).fadeIn(200);
-}
